a89c74c5f41b1586e07b09ccc82789aa688d37ae
yasarazizli <yasarazizli07@gmail.com>
2024-01-18 01:06:10 +0400

    first_commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" alt="" />
+      <img src={logo} alt="" />
+
+    </div>
+  );
+}
+
+export default App;
